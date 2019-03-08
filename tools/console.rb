b7ec9d9a44b4f48f5a399cd8fe4f72a9049b3d2b
Jessica Brumfield <jessicabrumfield@Jessicas-MacBook-Pro.local>
2019-03-08 11:02:27 -0600

    done

--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,9 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+m1 = Movie.new(title:"Red")
+v1 = Viewer.new(username:"dude28")
+q1 = QueueItem.new(viewer: v1,movie: m1, )
 
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
